775fc42811eeb555d3608b8d50d6c5a817d71d15
bianchettihugo <lhbianchetti@gmail.com>
2023-10-09 22:49:46 -0300

    [HGO-1005] - Created habit repository

---
+++ lib/app/habits/domain/repositories/habit_repository.dart
@@ -0,0 +1,12 @@
+import 'package:habit_go/app/habits/domain/entities/habit_entity.dart';
+import 'package:habit_go/core/utils/result.dart';
+
+abstract class HabitRepository {
+  Future<Result<HabitEntity>> createHabit(HabitEntity habit);
+
+  Future<Result<List<HabitEntity>>> readHabits();
+
+  Future<Result<HabitEntity>> updateHabit(HabitEntity habit);
+
+  Future<Result<HabitEntity>> deleteHabit(HabitEntity habit);
+}
---
+++ lib/core/utils/failure.dart
@@ -0,0 +1,35 @@
+class Failure {
+  final String message;
+  final String module;
+  final int statusCode;
+
+  const Failure({
+    this.message = '',
+    this.module = '',
+    this.statusCode = 0,
+  });
+
+  factory Failure.fromException(Exception exception, [String module = '']) {
+    return Failure(message: exception.toString(), module: module);
+  }
+
+  @override
+  bool operator ==(covariant Failure other) {
+    if (identical(this, other)) return true;
+
+    return other.message == message &&
+        other.module == module &&
+        other.statusCode == statusCode;
+  }
+
+  @override
+  int get hashCode => message.hashCode ^ module.hashCode ^ statusCode.hashCode;
+}
+
+class DatabaseFailure extends Failure {
+  const DatabaseFailure();
+}
+
+class CorruptedDataFailure extends Failure {
+  const CorruptedDataFailure();
+}
