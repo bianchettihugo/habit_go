d7726682eb6ab352d20b42a0e346447b68b62cb3
bianchettihugo <lhbianchetti@gmail.com>
2023-11-03 12:01:06 -0300

    [HGO-1061] - add settings cubit

---
+++ lib/app/settings/presentation/state/settings_cubit.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:habit_go/app/settings/domain/entities/settings_entity.dart';
+
+class SettingsCubit extends Cubit<SettingsEntity> {
+  SettingsCubit(super.initialState);
+
+  void updateTheme(ThemeMode themeMode) =>
+      emit(state.copyWith(themeMode: themeMode));
+
+  void updateCompleteAnimations(bool completeAnimations) =>
+      emit(state.copyWith(completeAnimations: completeAnimations));
+
+  void updateAppAnimations(bool appAnimations) =>
+      emit(state.copyWith(appAnimations: appAnimations));
+
+  void updateNotifications(bool notifications) =>
+      emit(state.copyWith(notifications: notifications));
+}
---
+++ test/app/settings/presentation/state/settings_cubit_test.dart
@@ -0,0 +1,53 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:habit_go/app/settings/domain/entities/settings_entity.dart';
+import 'package:habit_go/app/settings/presentation/state/settings_cubit.dart';
+
+void main() {
+  const themeMode = ThemeMode.dark;
+  const completeAnimations = true;
+  const appAnimations = false;
+  const notifications = true;
+
+  const settingsEntity = SettingsEntity(
+    themeMode: themeMode,
+    completeAnimations: completeAnimations,
+    appAnimations: appAnimations,
+    notifications: notifications,
+  );
+
+  blocTest<SettingsCubit, SettingsEntity>(
+    'settings/presentation/state - emits the correct state when updateTheme is called',
+    build: () => SettingsCubit(settingsEntity),
+    act: (cubit) => cubit.updateTheme(ThemeMode.light),
+    expect: () => [
+      settingsEntity.copyWith(themeMode: ThemeMode.light),
+    ],
+  );
+
+  blocTest<SettingsCubit, SettingsEntity>(
+    'settings/presentation/state - emits the correct state when updateCompleteAnimations is called',
+    build: () => SettingsCubit(settingsEntity),
+    act: (cubit) => cubit.updateCompleteAnimations(false),
+    expect: () => [
+      settingsEntity.copyWith(completeAnimations: false),
+    ],
+  );
+
+  blocTest<SettingsCubit, SettingsEntity>(
+    'settings/presentation/state - emits the correct state when updateAppAnimations is called',
+    build: () => SettingsCubit(settingsEntity),
+    act: (cubit) => cubit.updateAppAnimations(true),
+    expect: () => [
+      settingsEntity.copyWith(appAnimations: true),
+    ],
+  );
+
+  blocTest<SettingsCubit, SettingsEntity>(
+    'settings/presentation/state - emits the correct state when updateNotifications is called',
+    build: () => SettingsCubit(settingsEntity),
+    act: (cubit) => cubit.updateNotifications(false),
+    expect: () => [
+      settingsEntity.copyWith(notifications: false),
+    ],
+  );
+}
