36565d7fecba6352b6140f849d005cde7e511a34
bianchettihugo <lhbianchetti@gmail.com>
2023-11-03 11:38:22 -0300

    [HGO-1060] - add radio option widget

---
+++ test/core/widgets/radios/radio_option_test.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:habit_go/core/widgets/radios/radio_option.dart';
+
+import '../../../utils/test_utils.dart';
+
+void main() {
+  testWidgets('core/widgets/radios - renders the radio options',
+      (WidgetTester tester) async {
+    final options = {
+      'Option 1': 'option1',
+      'Option 2': 'option2',
+      'Option 3': 'option3',
+    };
+    String? selectedOption;
+
+    await tester.pumpWidgetWithApp(
+      RadioOption<String>(
+        options: options,
+        onChanged: (value) {
+          selectedOption = value;
+        },
+      ),
+    );
+
+    expect(find.byType(RadioOption<String>), findsOneWidget);
+    expect(find.byType(RadioListTile<String>), findsNWidgets(options.length));
+
+    for (final option in options.entries) {
+      final optionTile = find.byWidgetPredicate(
+        (widget) =>
+            widget is RadioListTile<String> &&
+            widget.title is Text &&
+            (widget.title! as Text).data == option.key,
+      );
+      expect(optionTile, findsOneWidget);
+    }
+
+    await tester.tap(find.byType(RadioListTile<String>).last);
+    await tester.pumpAndSettle();
+
+    expect(selectedOption, 'option3');
+  });
+}
